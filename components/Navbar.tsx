f084cb3e274122e6d2e1048d2e1ed3739c51918e
RizalPradanaaa <rizalpradanaaa@gmail.com>
2024-11-23 17:05:41 +0700

    navbar scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,12 +15,12 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between mx-auto mt-8 w-3/4 font-bold">
+    <div className="sticky py-4 top-0 z-50 flex items-center justify-between mx-auto w-3/4 font-bold bg-white/70 backdrop-blur-xs border-b-2  border-slate-200">
         <NavigationMenu>
                 <p className="font-bold italic">Write Sphere</p>
         </NavigationMenu>
-        <NavigationMenu className='hidden md:flex '>
-            <div className="flex font-bold">
+        <NavigationMenu className='hidden md:flex backdrop-blur-xs'>
+            <div className="flex font-bold gap-2">
                 <Link href="/" className={navigationMenuTriggerStyle()}>
                     Homepage
                 </Link>
